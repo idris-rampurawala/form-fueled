e3469a6f9861232dfe0ab1fdd0eaaf0662325b46
Idris Rampurawala <idris@connectedyou.io>
2021-05-16 20:26:03 +0530

    - implemented user jwt authetication - implemented user register api

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # form-fueled
 A web application to create and share questionnaire and get responses from any user.
+
+There are 2 seperate applications:
+- Django backend API
+- Angular frontend
--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -46,6 +47,12 @@ INSTALLED_APPS.extend([
     'django_extensions',
     'shell_plus',
 ])
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -137,3 +144,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+# JWT Auth Token Settings
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'UPDATE_LAST_LOGIN': True,
+}
